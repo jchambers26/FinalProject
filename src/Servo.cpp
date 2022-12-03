047e196ea369fb013672250c6e52a1defd7d07ac
MikeNikeCode <117119939+MikeNikeCode@users.noreply.github.com>
2022-12-03 02:17:48 -0700

    Update Servo.cpp

--- src/Servo.cpp
+++ src/Servo.cpp
@@ -1,7 +1,25 @@
-#include "Servo.h"
+#include "servo.h"
+#include <avr/io.h>
+#include "Arduino.h"
 
-void InitServo();
-void lock();
-void unlock();
+void Init_Servo(){
 
+  DDRH |= (1 << DDH5); // set as output
+  PORTH &= ~(1 << PH6); // set as low
+  
+}
 
+void lock_s(){
+
+    PORTH |= (1 << PH6);
+    delayMs(1800);    //position
+    PORTH &= ~(1 << PH6);
+    delayMs(18000);   //balance of 20000 cycle
+}
+void unlock_s(){
+
+    PORTH |= (1 << PH6);
+    delayMs(800);    //position
+    PORTH &= ~(1 << PH6);
+    delayMs(18600);   //balance of 20000 cycle
+}
