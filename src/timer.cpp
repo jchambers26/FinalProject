99245ae736631bfd23d9623837ba7bb06a2f0474
Jacob <chambersjacobj@gmail.com>
2022-12-07 14:40:48 -0700

    update timer

--- src/timer.cpp
+++ src/timer.cpp
@@ -11,6 +11,7 @@ void initTimer0(){
 
 }
 
+
 /* This delays the program an amount specified by unsigned int delay.
 * Use timer 0. Keep in mind that you need to choose your prescalar wisely
 * such that your timer is precise to 1 millisecond and can be used for
